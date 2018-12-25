ba1c995d2b8f7fc93af44ff42f5c0be0fc7535d8
erwin <erwin.paillacan@ug.uchile.cl>
2018-12-24 21:30:39 -0300

    Se agrega gitignore

--- train_CNN.py
+++ train_CNN.py
@@ -3,12 +3,12 @@ from data_utils_CNN import train_generator, val_batch_generator
 from model_utils_CNN import generar_modelo
 from train_utils import TensorBoardBatch
 from keras.callbacks import ModelCheckpoint
-
+from keras.callbacks import EarlyStopping
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
-nb_train_images = 226 # con cuantas imagenes se quiere entrenar
-batch_size = 16 #se toman de poco
-epocas= 5000 #cuantas veces se pasa cada batch por la red
+nb_train_images = 46000# con cuantas imagenes se quiere entrenar
+batch_size = 32 #se toman de poco
+epocas= 10000 #cuantas veces se pasa cada batch por la red
 model = generar_modelo(lr=1e-4)
 model.summary()
 
@@ -17,7 +17,9 @@ model.summary()
 tensorboard = TensorBoardBatch(batch_size=batch_size)
 checkpoint = ModelCheckpoint('weights/pesos_CNN_puro_mse.h5', monitor='loss', verbose=1,
                              save_best_only=True, save_weights_only=True)
-callbacks = [checkpoint, tensorboard]
+early_stopping =EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=500,
+                             verbose=0, mode='auto', baseline=None, restore_best_weights=False)
+callbacks = [checkpoint, early_stopping,tensorboard]
 
 gen=train_generator(batch_size)
 val=val_batch_generator(batch_size)
